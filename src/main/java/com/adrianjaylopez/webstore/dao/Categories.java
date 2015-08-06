762d993b627bfe005662dc9e2f122fb2899c9a7e
jayzamazing <adrianjaylopez@gmail.com>
2015-08-06 14:29:22 -0400

    Fixed some issues I had with bower, moved directory under resources/public to make it easier to access. Updated bower dependencies. Minor grammatic changes to classes. FIxed path issues in index with javascript libraries.

--- src/main/java/com/adrianjaylopez/webstore/dao/Categories.java
+++ src/main/java/com/adrianjaylopez/webstore/dao/Categories.java
@@ -1,21 +1,17 @@
 package com.adrianjaylopez.webstore.dao;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
 /**
  * This class represents the categories that the item belongs to.
- * @Author Adrian J Lopez
+ * @author Adrian J Lopez
  * @since <pre>6/21/15</pre>
  * @version 1.0
  */
-@Entity
+//@Entity
 public class Categories {
 
     //declare variables
-    @Id
-    @GeneratedValue
+    //@Id
+    //@GeneratedValue
     private Long id;
 
     private String category;
