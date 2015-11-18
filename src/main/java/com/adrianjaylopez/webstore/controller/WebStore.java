a5640c92e9acb5047667869b7733d9cb3a9013ca
jayzamazing <adrianjaylopez@gmail.com>
2015-11-17 20:53:06 -0500

    Changed webstore controller to use header to limit access to partial main.html page. Changed app.js to run header function at run. Added sendhead factory method to services.js to add header to all requests sent to server. Started slowly working on admin.html partial, added search field and button for it. Removed <base> tag from index.

--- src/main/java/com/adrianjaylopez/webstore/controller/WebStore.java
+++ src/main/java/com/adrianjaylopez/webstore/controller/WebStore.java
@@ -5,17 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * This class is basically going to act like the dispatcher servlet once everything is setup.
  *
  * @author Adrian J Lopez
  * @since <pre>6/14/15</pre>
- * @version 1.2
+ * @version 1.3
  */
 
 @Controller
@@ -29,22 +26,25 @@ public class WebStore {
         return "index";
     }
 
+    @RequestMapping(value = "/index")
+    public String index2() {return "index";}
+
     @RequestMapping(value = "/main")
-    public String main(){
-        return "main";
+    public String main(@RequestHeader(value = "Authorization", required = false) String auth){
+        if(auth != null && auth.compareTo("hs5533frehjlopop[]56") == 0)
+            return "main";
+        else
+            return "index";
     }
 
     @RequestMapping(value = "/admin")
-    public String admin(){
-        return "admin";
-    }
+    public String admin(){ return "admin"; }
 
     @RequestMapping(value = "/userInfo", method = RequestMethod.POST)
     public @ResponseBody User userInfo(@RequestBody User user) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
-        token = (UsernamePasswordAuthenticationToken) authProvider.authenticate(token);
-
-        user = (User) token.getPrincipal();
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+            token = (UsernamePasswordAuthenticationToken) authProvider.authenticate(token);
+            user = (User) token.getPrincipal();
 
         return user;
 
