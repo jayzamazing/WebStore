6c5cd66d257b2d45d9c2a163d891d7fdc9aab1d3
Jay <adrianjaylopez@gmail.com>
2015-12-01 21:31:05 -0500

    Removed application.properties from repo. Made some changes to the uploadcontroller, finished first iteration of upload mapping, still have to test. Minor documentation to imagerepository, minor change to variable in controllers.js. Added application.properties to .gitignore.

--- src/main/java/com/adrianjaylopez/webstore/controller/UploadController.java
+++ src/main/java/com/adrianjaylopez/webstore/controller/UploadController.java
@@ -1,17 +1,22 @@
 package com.adrianjaylopez.webstore.controller;
 
+import com.adrianjaylopez.webstore.dao.Image;
 import com.adrianjaylopez.webstore.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * This class is going to deal with uploading the images that will be used for the items pictures.
  * @author Adrian J Lopez
  * @since <pre>6/22/15</pre>
- * @version 1.2
+ * @version 1.3
  */
 @Controller
 public class UploadController {
@@ -19,9 +24,27 @@ public class UploadController {
     @Autowired
     UploadService uploadService;
 
+    /**
+     * Used to upload picture images to the database for storage.
+     * @param request holds the images and their metadata
+     * @return failed or success
+     */
     @RequestMapping(value="/upload", method= RequestMethod.POST)
-    public ResponseEntity fileUpload(){
+    public String fileUpload(MultipartHttpServletRequest request){
+        try {
+            //iterate over the request and grab the image names
+            for(Iterator<String> fileName = request.getFileNames(); request.getFileNames().hasNext();){
+                //get the file using the image name and set it to this file
+                MultipartFile file = request.getFile(fileName.next());
+                //create image object
+                Image image = image = new Image(file.getOriginalFilename(), file.getBytes(), file.getContentType());
+                //pass in image to upload service for storage
+                uploadService.uploadImage(image);
 
-        return null;//TODO complete when fully implemented
+            }
+        } catch (IOException e) {
+        return "failed";
+    }
+        return "success";
     }
 }
